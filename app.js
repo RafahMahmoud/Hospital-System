de19fe926066799b3a963696b993ffa1a8cb1b1c
RafahMahmoud <rafah.shreem98@gmail.com>
2024-06-22 14:20:09 +0300

    solve task of 11-june

--- app.js
+++ app.js
@@ -1,3 +1,109 @@
+function validateName() {
+    const fname = document.getElementById('fname').value;
+
+    // Regular expressions for each criteria
+    const nospacesRegex =   /^([A-z0-9!@#$%^&*().,<>{}[\]<>?_=+\-|;:\'\"\/])*[^\s]\1*$/;
+    
+
+    // Validate each criteria
+    validateCriteria(nospacesRegex, 'nospaces', fname);
+
+}
+
+function validateCriteria(regex, elementId, fname) {
+    const element = document.getElementById(elementId);
+    if (regex.test(fname)) {
+        element.classList.remove('invalid');
+        element.classList.add('valid');
+    } else {
+        element.classList.remove('valid');
+        element.classList.add('invalid');
+    }
+}
+// let isUserExist = array.some(patient => patient.FullName === FullName);
+//     if (isUserExist) {
+//         alert("User already exists. Please use a different name.");
+//         return;}
+        function validatePassword() {
+            const password = document.getElementById('password').value;
+
+            // Regular expressions for each criteria
+            const lengthRegex = /.{8,}/;
+            const lowercaseRegex = /[a-z]/;
+            const uppercaseRegex = /[A-Z]/;
+            const digitRegex = /\d/;
+            const specialRegex = /[!@#$%^&*(),.?":{}|<>]/;
+
+            // Validate each criteria
+            validateCriteria(lengthRegex, 'length', password);
+            validateCriteria(lowercaseRegex, 'lowercase', password);
+            validateCriteria(uppercaseRegex, 'uppercase', password);
+            validateCriteria(digitRegex, 'digit', password);
+            validateCriteria(specialRegex, 'special', password);
+        }
+
+        function validateCriteria(regex, elementId, password) {
+            const element = document.getElementById(elementId);
+            if (regex.test(password)) {
+                element.classList.remove('invalid');
+                element.classList.add('valid');
+            } else {
+                element.classList.remove('valid');
+                element.classList.add('invalid');
+            }
+        }
+       
+        function validateEmail() {
+            const email = document.getElementById('email').value;
+
+            // Regular expressions for each criteria
+            const emailRegex =  /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+            
+            // Validate each criteria
+            validateCriteria(emailRegex, 'emailvalid', email);
+
+        }
+
+        function validateCriteria(regex, elementId,email) {
+            const element = document.getElementById(elementId);
+            if (regex.test(email)) {
+                element.classList.remove('invalid');
+                element.classList.add('valid');
+            } else {
+                element.classList.remove('valid');
+                element.classList.add('invalid');
+            }
+        }
+        function validateNumber() {
+            const phone = document.getElementById('phone').value;
+
+            // Regular expressions for each criteria
+            const typePhoneRegex = /^[0-9]{10}$/;
+            const startPhoneRegex = /^\b07/;
+            // const lengthPhoneRegex = /.{10}$/;
+            
+            // Validate each criteria
+            validateCriteria(typePhoneRegex, 'typePhone', phone);
+            validateCriteria(startPhoneRegex, 'startPhone', phone);
+           
+
+        }
+
+        function validateCriteria(regex, elementId,phone) {
+            const element = document.getElementById(elementId);
+            if (regex.test(phone)) {
+                element.classList.remove('invalid');
+                element.classList.add('valid');
+            } else {
+                element.classList.remove('valid');
+                element.classList.add('invalid');
+            }
+        }
+
+       
+
+
+
 let array = [];
 
 function Person(fullName, userPassword, birthDate, phoneNum, userGender, userDisease) {
